4e9ab890ff5101255fdffac881c0c143f2162788
Viviana Feijoo <vivfeijoo@gmail.com>
2025-04-29 21:45:34 +0000

    symbol

--- src/app.js
+++ src/app.js
@@ -6,25 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-window.onload = function() {
+window.onload = function () {
   const suits = ['heart', 'spade', 'club', 'diamond'];
   const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-  
-  const randomSuitIndex = Math.floor(Math.random() * suits.length);
-  const randomSuit = suits[randomSuitIndex];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const suitSymbol = getSuitSymbol(randomSuit);
 
-  
-  const randomValueIndex = Math.floor(Math.random() * values.length);
-  const randomValue = values[randomValueIndex];
-
-  
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = `
     <div class="card ${randomSuit}">
-      ${randomValue} 
-      <br/>
-      <span class="suit">${getSuitSymbol(randomSuit)}</span>
+      <div class="top-suit">${suitSymbol}</div>
+      <div class="center-value">${randomValue}</div>
+      <div class="bottom-suit">${suitSymbol}</div>
     </div>
   `;
 };
